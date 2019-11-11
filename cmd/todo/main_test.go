ff4f6d71edcff25bf7d56392e8dcb948b337421a
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2019-11-11 20:16:42 +0200

    Add tests for CLI

---
+++ cmd/todo/main_test.go
@@ -0,0 +1,64 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"testing"
+)
+
+const (
+	todoFileName = ".todo.json"
+	binName      = "todo"
+)
+
+func TestMain(m *testing.M) {
+	if _, err := os.Stat(binName); os.IsNotExist(err) {
+		build := exec.Command("go", "build", "-o", binName)
+		if err := build.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot build %s: %s", binName, err)
+			os.Exit(1)
+		}
+	}
+
+	result := m.Run()
+
+	//	os.Remove(binName)
+	//	os.Remove(todoFileName)
+
+	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	task := "testTask"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := path.Join(dir, binName)
+
+	t.Run("AddNewTask", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := task + "\n"
+
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead", expected, string(out))
+		}
+
+	})
+}
